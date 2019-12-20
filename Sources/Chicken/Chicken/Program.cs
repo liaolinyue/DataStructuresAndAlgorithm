327f208a44f01bb415d4fa23173d05c66fe2ffb4
liaolinyue <lll123456>
2019-12-20 11:02:45 +0800

    Chicken

--- Sources/Chicken/Chicken/Program.cs
+++ Sources/Chicken/Chicken/Program.cs
@@ -6,16 +6,16 @@ namespace Chicken
     {
         public static void Main(string[] args)
         {
-            for (var x = 0; x <= 20; x++)
+            for (int x = 0; x <= 20; x++)
             {
-                for (var y = 0; y <= 33; y++)
+                for (int y = 0; y <= 100 / 3; y++)
                 {
-                    for (var z = 0; z <= 100; z++)
-                        if ((5 * x + 3 * y + z / 3) == 100 && (x + y + z) == 100)
-                        {
-                            Console.WriteLine("公鸡有{0}只,母鸡有{1}只,小鸡有{2}只",x,y,z);
+                    int z = 100 - x - y;
+                    if (5 * x + 3 * y + z / 3 == 100 && z % 3 == 0)
+                    {
+                        Console.WriteLine("公鸡有{0}只,母鸡有{1}只,小鸡有{2}只", x, y, z);
 
-                        }
+                    }
                 }
             }
 
